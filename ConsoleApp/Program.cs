c19b053a2fbc79aa5d83be6cae5bca731103260e
Mishleyn <svalov1985@bk.ru>
2024-05-17 14:07:19 +0700

    Почистил файлы

--- ConsoleApp/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using Calculator;
-using ShapesCalculator.Shapes;
-
-Circle circle = new Circle(7);
-Triangle triangle = new Triangle(7, 25, 24);
-Calculator.Calculator calculator = new Calculator.Calculator();
-Console.WriteLine(triangle.IsRightAngled());
