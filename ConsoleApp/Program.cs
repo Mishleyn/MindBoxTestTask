760908f05e535c722d8b23fb131155c0e108b35c
Mishleyn <svalov1985@bk.ru>
2024-05-17 13:20:06 +0700

    feature: Написал юнит тесты

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
-﻿using TestTask;
+﻿using Calculator;
+using ShapesCalculator.Shapes;
 
 Circle circle = new Circle(7);
 Triangle triangle = new Triangle(7, 25, 24);
-Calculator calculator = new Calculator();
+Calculator.Calculator calculator = new Calculator.Calculator();
 Console.WriteLine(triangle.IsRightAngled());
